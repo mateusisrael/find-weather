00f91da13878c72ddc13572f3574c0d3bc24a4c5
mateusisrael <mateuss.i.fernandes@gmail.com>
2023-04-29 17:14:30 -0300

    Adicionando constantes de estilos gerais

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,44 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
+const gray100 = "#AFAFAF"
+const gray200 = "#A1A1A1"
+const gray300 = "#838384"
+const gray500 = "#6F7072"
+const gray400 = "#7E7E7E"
+const gray600 = "#4D4D4D"
+const dark = "#1B1D22"
+const dark100 = "#33383E"
+const dark300 = "#23262B"
+const dark400 = "#212428"
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    text: '#fff',
+    background: dark,
+    tint: tintColorDark,
+    textSecondary: gray100,
+    border: gray300,
+    button: dark300,
+    tabTextSelected: gray500,
+    tabTextDefault: "#fff",
+    searchbarText: gray200,
+    climateStatus: gray400,
+    climateBorder: gray600,
+    card: dark100,
   },
   dark: {
     text: '#fff',
-    background: '#1B1D22',
+    background: dark,
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+    textSecondary: gray100,
+    border: gray300,
+    button: dark300,
+    tabTextSelected: gray500,
+    tabTextDefault: "#fff",
+    searchbarText: gray200,
+    climateDescriptionText: gray400,
+    climateBackground: dark400,
+    climateBorder: gray600,
+    card: dark100,
   },
 };
